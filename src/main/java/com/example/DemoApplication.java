af03a8b57161e3abb2f34493068e832963447294
Leonardo Campan <leonardo.campan@icloud.com>
2023-04-04 14:33:51 +0200

    DemoApplication aggiornamento

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,16 +1,7 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.annotation.Bean;
-
-import com.example.configuration.WebsocketSourceConfiguration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 
